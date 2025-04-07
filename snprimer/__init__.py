099c2fd12378a95859454b266099b84cb872b201
hucinoo <139854353+hucinoo@users.noreply.github.com>
2025-04-07 16:05:36 +0200

    Update __init__.py

--- snprimer/__init__.py
+++ snprimer/__init__.py
@@ -2,3 +2,5 @@ from .position_range import PositionRange
 from .primer import Primer
 from .primer_maker import PrimerMaker
 from .primer_pair import PrimerPair
+from .txt_formatter import parse_genomic_data
+print("%s: I was imported from %s" %(__name__, __file__))
